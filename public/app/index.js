ddbc40f543c4d9bf72c0b58d4b002eee6551bc8b
緋 <win87525@gmail.com>
2023-05-03 01:40:40 +0800

    做好sec2的zindex效果

--- public/app/index.js
+++ public/app/index.js
@@ -1,60 +1,213 @@
 $(document).ready(function () {
     // 啟動aos的
     AOS.init();
+
+    // 熱門商品抽頁效果
+    $('#picimg1').click(function () {
+        $('#text2').fadeOut(0);
+        $('#text3').fadeOut(0);
+        $('#text4').fadeOut(0);
+        $('#text5').fadeOut(0);
+        $('#text1').fadeIn(800);
+        $(this).css("left", "60%");
+        setTimeout(() => {
+            $(this).css({
+                "left": "0%",
+                "zIndex": "12"
+            }); 
+            $("#picimg2").css({
+                "left": "11%",
+                "zIndex": "9"
+            });
+            $("#picimg3").css({
+                "left": "22%",
+                "zIndex": "8"
+            });
+            $("#picimg4").css({
+                "left": "32%",
+                "zIndex": "7"
+            });
+            $("#picimg5").css({
+                "left": "42%",
+                "zIndex": "6"
+            });
+            
+        }, 400);
+    });
+    $('#picimg2').click(function () {
+        $('#text1').fadeOut(0);
+        $('#text3').fadeOut(0);
+        $('#text4').fadeOut(0);
+        $('#text5').fadeOut(0);
+        $('#text2').fadeIn(1200);
+        $(this).css("left", "60%");
+        setTimeout(() => {
+            $(this).css({
+                "left": "11%",
+                "zIndex": "12"
+            }); 
+            $("#picimg1").css({
+                "left": "0%",
+                "zIndex": "10"
+            });
+            $("#picimg3").css({
+                "left": "22%",
+                "zIndex": "8"
+            });
+            $("#picimg4").css({
+                "left": "32%",
+                "zIndex": "7"
+            });
+            $("#picimg5").css({
+                "left": "42%",
+                "zIndex": "6"
+            });
+            
+        }, 400);
+    });
+    $('#picimg3').click(function () {
+        $('#text1').fadeOut(0);
+        $('#text2').fadeOut(0);
+        $('#text4').fadeOut(0);
+        $('#text5').fadeOut(0);
+        $('#text3').fadeIn(1200);
+        $(this).css("left", "60%");
+        setTimeout(() => {
+            $(this).css({
+                "left": "22%",
+                "zIndex": "12"
+            }); 
+            $("#picimg1").css({
+                "left": "0%",
+                "zIndex": "10"
+            });
+            $("#picimg2").css({
+                "left": "11%",
+                "zIndex": "9"
+            });
+            $("#picimg4").css({
+                "left": "32%",
+                "zIndex": "7"
+            });
+            $("#picimg5").css({
+                "left": "42%",
+                "zIndex": "6"
+            });
+            
+        }, 400);
+    });
+    $('#picimg4').click(function () {
+        $('#text1').fadeOut(0);
+        $('#text2').fadeOut(0);
+        $('#text3').fadeOut(0);
+        $('#text5').fadeOut(0);
+        $('#text4').fadeIn(800);
+        $(this).css("left", "70%");
+        setTimeout(() => {
+            $(this).css({
+                "left": "32%",
+                "zIndex": "12"
+            }); 
+            $("#picimg1").css({
+                "left": "0%",
+                "zIndex": "10"
+            });
+            $("#picimg3").css({
+                "left": "22%",
+                "zIndex": "8"
+            });
+            $("#picimg2").css({
+                "left": "11%",
+                "zIndex": "9"
+            });
+            $("#picimg5").css({
+                "left": "42%",
+                "zIndex": "6"
+            });
+            
+        }, 400);
+    });
+    $('#picimg5').click(function () {
+        $('#text1').fadeOut(0);
+        $('#text2').fadeOut(0);
+        $('#text3').fadeOut(0);
+        $('#text4').fadeOut(0);
+        $('#text5').fadeIn(800);
+        $(this).css("left", "70%");
+        setTimeout(() => {
+            $(this).css({
+                "left": "42%",
+                "zIndex": "12"
+            }); 
+            $("#picimg1").css({
+                "left": "0%",
+                "zIndex": "10"
+            });
+            $("#picimg3").css({
+                "left": "22%",
+                "zIndex": "8"
+            });
+            $("#picimg4").css({
+                "left": "32%",
+                "zIndex": "7"
+            });
+            $("#picimg2").css({
+                "left": "11%",
+                "zIndex": "9"
+            });
+            
+        }, 400);
+    });
+
+
+
+
     //新聞列的bar按鈕
-    $(document).ready(function () {
-        $('#newsbut').click(function () {
-            $('#newsButs').addClass("butfocus");
-            $('#renewButs').removeClass("butfocus");
-            $('#discountButs').removeClass("butfocus");
-            $('#productButs').removeClass("butfocus");
-    
-            $("#renewAll").fadeOut(300);
-            $("#discountAll").fadeOut(300);
-            $("#productAll").fadeOut(300);
-            $('#newsAll').fadeIn(800);
-        });
+    $('#newsbut').click(function () {
+        $('#newsButs').addClass("butfocus");
+        $('#renewButs').removeClass("butfocus");
+        $('#discountButs').removeClass("butfocus");
+        $('#productButs').removeClass("butfocus");
+
+        $("#renewAll").fadeOut(300);
+        $("#discountAll").fadeOut(300);
+        $("#productAll").fadeOut(300);
+        $('#newsAll').fadeIn(800);
     });
-    $(document).ready(function () {
-        $('#renewbut').click(function () {
-            $("#newsAll").fadeOut(300);
-            $("#productAll").fadeOut(300);
-            $("#discountAll").fadeOut(300);
-            $('#renewAll').fadeIn(800);
-    
-            $('#renewButs').addClass("butfocus");
-            $('#newsButs').removeClass("butfocus");
-            $('#discountButs').removeClass("butfocus");
-            $('#productButs').removeClass("butfocus");
-        });
+    $('#renewbut').click(function () {
+        $("#newsAll").fadeOut(300);
+        $("#productAll").fadeOut(300);
+        $("#discountAll").fadeOut(300);
+        $('#renewAll').fadeIn(800);
+
+        $('#renewButs').addClass("butfocus");
+        $('#newsButs').removeClass("butfocus");
+        $('#discountButs').removeClass("butfocus");
+        $('#productButs').removeClass("butfocus");
     });
-    $(document).ready(function () {
-        $('#discountbut').click(function () {
-            $("#newsAll").fadeOut(300);
-            $("#productAll").fadeOut(300);
-            $("#renewAll").fadeOut(300);
-            $('#discountAll').fadeIn(800);
-    
-            $('#discountButs').addClass("butfocus");
-            $('#newsButs').removeClass("butfocus");
-            $('#renewButs').removeClass("butfocus");
-            $('#productButs').removeClass("butfocus");
-        });
+    $('#discountbut').click(function () {
+        $("#newsAll").fadeOut(300);
+        $("#productAll").fadeOut(300);
+        $("#renewAll").fadeOut(300);
+        $('#discountAll').fadeIn(800);
+
+        $('#discountButs').addClass("butfocus");
+        $('#newsButs').removeClass("butfocus");
+        $('#renewButs').removeClass("butfocus");
+        $('#productButs').removeClass("butfocus");
     });
-    $(document).ready(function () {
-        $('#productbut').click(function () {
-            $("#newsAll").fadeOut(300);
-            $("#discountAll").fadeOut(300);
-            $("#renewAll").fadeOut(300);
-            $('#productAll').fadeIn(800);
-    
-            $('#productButs').addClass("butfocus");
-            $('#newsButs').removeClass("butfocus");
-            $('#renewButs').removeClass("butfocus");
-            $('#discountButs').removeClass("butfocus");
-        });
+
+    $('#productbut').click(function () {
+        $("#newsAll").fadeOut(300);
+        $("#discountAll").fadeOut(300);
+        $("#renewAll").fadeOut(300);
+        $('#productAll').fadeIn(800);
+
+        $('#productButs').addClass("butfocus");
+        $('#newsButs').removeClass("butfocus");
+        $('#renewButs').removeClass("butfocus");
+        $('#discountButs').removeClass("butfocus");
     });
-    
 
 });
 
